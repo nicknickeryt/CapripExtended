a83831562ebe43fdf541faf459e31da35dc05ee5
NickNicker <nick4nicker@gmail.com>
2022-11-15 21:59:36 +0100

    hopefully it works

--- drivers/input/touchscreen/nt36xxx/nt36xxx.h
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.h
@@ -83,7 +83,7 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 extern const uint16_t gesture_key_array[];
 #endif
 #define BOOT_UPDATE_FIRMWARE 1
-#define BOOT_UPDATE_FIRMWARE_NAME "novatek_ts_fw.bin"
+#define BOOT_UPDATE_FIRMWARE_NAME "nt36525c_novatek_ts_fw.bin"
 
 //---ESD Protect.---
 #define NVT_TOUCH_ESD_PROTECT 0
