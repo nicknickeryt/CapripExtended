5cd9ba346ff8c9d8bb17b078cf364186c0bbd30a
NickNicker <nick4nicker@gmail.com>
2023-04-08 23:43:35 +0200

    Revert "fscrypt: add fscrypt_is_nokey_name()"
    This reverts commit 159e1de201b6fca10bfec50405a3b53a561096a8.

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -114,7 +114,11 @@ fscrypt_get_dummy_context(struct super_block *sb)
  */
 static inline void fscrypt_handle_d_move(struct dentry *dentry)
 {
+<<<<<<< HEAD
 	dentry->d_flags &= ~DCACHE_ENCRYPTED_NAME;
+=======
+	dentry->d_flags &= ~DCACHE_NOKEY_NAME;
+>>>>>>> parent of 159e1de201b6 (fscrypt: add fscrypt_is_nokey_name())
 }
 
 /* crypto.c */
@@ -249,13 +253,10 @@ fscrypt_get_dummy_context(struct super_block *sb)
 	return NULL;
 }
 
-<<<<<<< HEAD
 static inline void fscrypt_handle_d_move(struct dentry *dentry)
 {
 }
 
-=======
->>>>>>> parent of 159e1de201b6 (fscrypt: add fscrypt_is_nokey_name())
 /* crypto.c */
 static inline void fscrypt_enqueue_decrypt_work(struct work_struct *work)
 {
