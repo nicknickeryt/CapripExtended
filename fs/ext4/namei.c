2b0bcd514ac3cf9d679b54937aa5186565226ac4
NickNicker <nick4nicker@gmail.com>
2023-04-10 10:59:54 +0200

    revert: fscrypt: add fscrypt_is_nokey_name()

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -2304,8 +2304,11 @@ static int ext4_add_entry(handle_t *handle, struct dentry *dentry,
 	if (!dentry->d_name.len)
 		return -EINVAL;
 
-	if (fscrypt_is_nokey_name(dentry))
-		return -ENOKEY;
+#ifdef CONFIG_UNICODE
+	if (sb_has_enc_strict_mode(sb) && IS_CASEFOLDED(dir) &&
+	    sb->s_encoding && utf8_validate(sb->s_encoding, &dentry->d_name))
+		return -EINVAL;
+#endif
 
 	retval = ext4_fname_setup_filename(dir, &dentry->d_name, 0, &fname);
 	if (retval)
